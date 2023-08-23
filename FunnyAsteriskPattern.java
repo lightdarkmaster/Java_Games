bd5ee5cc89e2422015c317172556584f139a7323
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-08-23 20:11:36 +0800

    Add Smiley Face Java

---
+++ FunnyAsteriskPattern.java
@@ -0,0 +1,11 @@
+public class FunnyAsteriskPattern {
+    public static void main(String[] args) {
+        // Create a funny smiley face using asterisks
+        System.out.println("   *****   ");
+        System.out.println(" *       * ");
+        System.out.println("*  O   O  *");
+        System.out.println("*    ^    *");
+        System.out.println(" * \\___/ * ");
+        System.out.println("   *****   ");
+    }
+}
